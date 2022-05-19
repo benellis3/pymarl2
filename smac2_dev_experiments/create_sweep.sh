f4ff27a99bd47fb0c542be9c56e0cfa8447bb884
Skander Moalla <skan.moalla@gmail.com>
2022-05-19 11:06:07 +0100

    Refactor.

--- smac2_dev_experiments/create_sweep.sh
+++ smac2_dev_experiments/create_sweep.sh
@@ -5,3 +5,5 @@
 SWEEP_NAME=$1
 ./smac2_dev_experiments/run_in_docker.sh 0 wandb sweep smac2_dev_experiments/experiments/obs_masking_effects/$SWEEP_NAME.yaml
 # Then get the sweep id in docker logs.
+sleep 3
+docker logs $(docker ps -a | sed -n 2p | grep -oE '[^ ]+$')
