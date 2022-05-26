31209e01105b6e9fd006247b11da7d8da1309403
Skander Moalla <skan.moalla@gmail.com>
2022-05-26 10:27:35 +0100

    Add smac2.

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo 'Building Dockerfile with image name pymarl'
-docker build --no-cache --build-arg UID=$UID -t pymarl:ms21sm_pymarl2 .
+docker build --no-cache --build-arg UID=$UID -t pymarl:ms21sm_smac_v2 .
--- docker/install_dependencies.sh
+++ docker/install_dependencies.sh
@@ -7,4 +7,7 @@
 pip install --upgrade pip
 pip install --ignore-installed six
 pip install sacred numpy scipy gym==0.10.8 matplotlib seaborn pyyaml pygame pytest probscale imageio snakeviz tensorboard-logger wandb tqdm
-pip install git+https://github.com/skandermoalla/smac.git@smac-v2-feature-names
+# SMAC v1
+# pip install git+https://github.com/skandermoalla/smac.git@feature-names
+# SMAC v2
+pip install git+https://github.com/benellis3/smac.git@smac-v2-feature-names
