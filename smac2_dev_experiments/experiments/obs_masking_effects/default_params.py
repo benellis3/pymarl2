2165ba3de3f7872c577b5ce119e66ab9c4df49a6
Skander Moalla <skan.moalla@gmail.com>
2022-05-26 18:26:25 +0100

    Add smac2.

--- smac2_dev_experiments/experiments/obs_masking_effects/default_params.py
+++ smac2_dev_experiments/experiments/obs_masking_effects/default_params.py
@@ -6,7 +6,7 @@ launch_time = time.strftime("%Y-%m-%d_%H-%M-%S")
 params = dict(
     project="SMAC2-masking-runs",
     entity="oxwhirl",
-    group="dev-pymarl2-smac1",
+    group="dev-pymarl2-smac2",
     launch_time=launch_time,
     save_best=True,
     eval_every=5,
@@ -15,8 +15,11 @@ params = dict(
     epochs=500,
     batch_size=512,
     learning_rate=0.005,
-    map_name="3s_vs_5z",
-    mask_name="ally_health",
+    smac_version=2,
+    map_name="gen_protoss",
+    nb_units=20,
+    mask_name="nothing",
+    seed=0
 )
 
 
