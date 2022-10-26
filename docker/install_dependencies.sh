11ca20226e13e4b52299c0615dd99616bfaa30ae
benellis3 <benellis@robots.ox.ac.uk>
2022-10-26 10:42:53 +0100

    Update dependencies

--- docker/install_dependencies.sh
+++ docker/install_dependencies.sh
@@ -12,5 +12,5 @@ then
   pip install git+https://github.com/skandermoalla/smac.git@feature-names
 else
   # SMACv2
-  pip install git+https://github.com/benellis3/smac.git@smac-v2-feature-names
+  pip install git+https://github.com/oxwhirl/smacv2.git@ranges
 fi
